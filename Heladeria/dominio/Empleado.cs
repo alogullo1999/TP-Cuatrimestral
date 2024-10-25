6770120741e612465f4843bf01704e5c2e93eafb
FrancoBonzi <bon.franco12@gmail.com>
2024-10-24 23:30:49 -0300

    dominio: class Proveedor, class Empleado, class Voucher

---
+++ Heladeria/dominio/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        public string Dni { get; set; }
+
+        public string Ciudad { get; set; }
+    }
+}
---
+++ Heladeria/dominio/Proveedor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Proveedor
+    {
+        public int IdProveedor { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Email { get; set; }
+
+        public string Dni { get; set; }
+
+        public string Ciudad { get; set; }
+    }
+}
