df9b7777a00a96732913742fa1956d7708fed822
FrancoBonzi <bon.franco12@gmail.com>
2024-10-25 12:22:17 -0300

    Creación negocio(class library) y ProductoNegocio(negocio)

---
+++ Heladeria/negocio/ProductoNegocio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using dominio;
+using negocio;
+
+namespace negocio
+{
+    public class ProductoNegocio
+    {
+    }
+}
