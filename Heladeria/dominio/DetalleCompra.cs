e29ed3b95aae10d3180a713be4352681e94440d6
FrancoBonzi <bon.franco12@gmail.com>
2024-12-13 12:37:04 -0300

    commit: creación class DetalleCompra

---
+++ Heladeria/dominio/DetalleCompra.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class DetalleCompra
+    {
+        public DateTime FechaCompra { get; set; }
+
+        public int IdCompra { get; set; }
+
+        public int IdProveedor { get; set; }
+
+        public int IdProducto { get; set; }
+    }
+}
